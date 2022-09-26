64e7539330be7ac49bd03f03e12ddfded3362d0c
estebanSaez <85312860+estebansaez@users.noreply.github.com>
2022-09-26 20:02:26 -0300

    Add files via upload

---
+++ Microondas.java
@@ -0,0 +1,32 @@
+public class Microondas {
+    String color;
+    String marca;
+    int tiempo;
+    int temperatura;
+
+    public int defTemperatura(){
+        //Definicion de la temperatura a la que se calentara el alimento
+    }
+
+    public int defTiempo(){
+        //Definicion del tiempo que se calentara el alimento
+    }
+
+    public void girarBase(){
+
+    }
+
+    public void obtenerTemp(){
+
+    }
+
+    public void iniciar(){
+
+    }
+
+    public void detener(){
+
+    }
+}
+
+
